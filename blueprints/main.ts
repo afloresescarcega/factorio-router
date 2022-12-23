25eab6501bcf876f34a8cc66f6ca068a162328b6
Alex Flores Escarcega <afloresescarcega@users.noreply.github.com>
2022-12-23 14:34:54 -0600

    checkpoint create multiples

--- blueprints/main.ts
+++ blueprints/main.ts
@@ -5,9 +5,10 @@ import DEFAULT_ENTITIES from "https://esm.sh/v87/factorio-blueprint@2.4.0/es2022
 
 
 let myBlueprint: InstanceType<typeof Blueprint> = new Blueprint();
+let pos:InstanceType<typeof Blueprint.position> = {x: 0, y: 0};
 myBlueprint.createEntity("assembling_machine_1", { x: 0, y: 0 }, Blueprint.UP);
 
-add_inserters(DEFAULT_ENTITIES.getEntityData().assembling_machine_1, myBlueprint);
+add_inserters(DEFAULT_ENTITIES.getEntityData().assembling_machine_1, myBlueprint, pos);
 console.log(myBlueprint.encode());
 
 
@@ -19,13 +20,14 @@ export function validate_io_orientations(entity: InstanceType<typeof Blueprint.e
     return true
 }
 
-export function add_inserters(entity: InstanceType<typeof Blueprint.entity>, blueprint: InstanceType<typeof Blueprint>, orientations?:string[]) {
+export function add_inserters(entity: InstanceType<typeof Blueprint.entity>, blueprint: InstanceType<typeof Blueprint>, position:InstanceType<typeof Blueprint.position>, orientations?:string[]) {
     if (orientations) {
         validate_io_orientations(entity, orientations);
     }
 
-    blueprint.createEntity("medium_electric_pole", { x: -1, y: -1 }, Blueprint.DOWN); // top left corner
-    blueprint.createEntity("medium_electric_pole", { x: entity.width, y: -1 }, Blueprint.DOWN); // top right corner
+    console.log("entity's pos: " + position.x + " " + position.y);
+    blueprint.createEntity("medium_electric_pole", { x: position.x - 1, y: position.y -1 }, Blueprint.DOWN); // top left corner
+    blueprint.createEntity("medium_electric_pole", { x: position.x + entity.width, y: position.y - 1 }, Blueprint.DOWN); // top right corner
     // clockwise starting from top, ending at left
 
     let orientations_index = 0;
@@ -33,9 +35,9 @@ export function add_inserters(entity: InstanceType<typeof Blueprint.entity>, blu
         if(orientations && orientations[orientations_index] !== "")
         {
             if(orientations[orientations_index] === 'o') {
-                blueprint.createEntity("inserter", {x: x_i, y: -1}, Blueprint.DOWN); // top
+                blueprint.createEntity("inserter", {x: x_i + position.x, y: -1 + position.y}, Blueprint.DOWN); // top
             } else if(orientations[orientations_index] === 'i'){
-                blueprint.createEntity("inserter", {x: x_i, y: -1}, Blueprint.UP); // top
+                blueprint.createEntity("inserter", {x: x_i + position.x, y: -1 + position.y}, Blueprint.UP); // top
             }
         }
         orientations_index++;
@@ -45,9 +47,9 @@ export function add_inserters(entity: InstanceType<typeof Blueprint.entity>, blu
         if(orientations && orientations[orientations_index] !== "")
         {
             if(orientations[orientations_index] === 'o') {
-                blueprint.createEntity("inserter", { x: entity.width, y: y_i }, Blueprint.LEFT); // right
+                blueprint.createEntity("inserter", { x: entity.width + position.x, y: y_i + position.y }, Blueprint.LEFT); // right
             } else if(orientations[orientations_index] === 'i'){
-                blueprint.createEntity("inserter", { x: entity.width, y: y_i }, Blueprint.RIGHT); // right
+                blueprint.createEntity("inserter", { x: entity.width + position.x, y: y_i + position.y }, Blueprint.RIGHT); // right
             }
         }
         orientations_index++;
@@ -56,9 +58,9 @@ export function add_inserters(entity: InstanceType<typeof Blueprint.entity>, blu
     for (let x_i = 0; x_i < entity.width; x_i++) {
         if(orientations && orientations[orientations_index] !== ""){
             if(orientations[orientations_index] === 'o') {
-                blueprint.createEntity("inserter", { x: x_i, y: entity.height }, Blueprint.UP); // bottom
+                blueprint.createEntity("inserter", { x: x_i + position.x, y: entity.height + position.y }, Blueprint.UP); // bottom
             } else if(orientations[orientations_index] === 'i'){
-                blueprint.createEntity("inserter", { x: x_i, y: entity.height }, Blueprint.DOWN); // bottom
+                blueprint.createEntity("inserter", { x: x_i + position.x, y: entity.height  + position.y}, Blueprint.DOWN); // bottom
             }
         }
         orientations_index++;
@@ -67,9 +69,9 @@ export function add_inserters(entity: InstanceType<typeof Blueprint.entity>, blu
     for (let y_i = 0; y_i < entity.height; y_i++) {
         if(orientations && orientations[orientations_index] !== ""){
             if(orientations[orientations_index] === 'o') {
-                blueprint.createEntity("inserter", { x: -1, y: y_i }, Blueprint.RIGHT); // left
+                blueprint.createEntity("inserter", { x: -1 + position.x, y: y_i + position.y }, Blueprint.RIGHT); // left
             } else if(orientations[orientations_index] === 'i'){
-                blueprint.createEntity("inserter", { x: -1, y: y_i }, Blueprint.LEFT); // left
+                blueprint.createEntity("inserter", { x: -1 + position.x, y: y_i + position.y }, Blueprint.LEFT); // left
             }
         }
         orientations_index++;
--- blueprints/main_test.ts
+++ blueprints/main_test.ts
@@ -12,7 +12,25 @@ Deno.test("validate add_inserters middles are inputs else outputs", () => {
     let myBlueprint: InstanceType<typeof Blueprint> = new Blueprint();
     myBlueprint.createEntity("assembling_machine_1", { x: 0, y: 0 }, Blueprint.UP);
     let orientations_ideal = ["o", "i", "o", "o", "i", "o", "o", "i", "o", "o", "i", "o"];
-    add_inserters(DEFAULT_ENTITIES.getEntityData().assembling_machine_1, myBlueprint, orientations_ideal);
+    let pos:InstanceType<typeof Blueprint.position> = {x: 0, y: 0};
+    add_inserters(DEFAULT_ENTITIES.getEntityData().assembling_machine_1, myBlueprint, pos, orientations_ideal);
+    let ideal_output = "0eJy9lttugzAMht/F1yCR0JWWy71GVVVArdZSSFAI1SrEuy+BburK1hQNcUkw9hcfftxCLhqsNEkDaQtUKFlDumuhppPMhDsz1wohBTJYQgAyK91TVtdY5oLkKSyz4kwSQwZdACSP+AEp6wKvixKP1JQhCiyMpiKslMA7F7zbB4DSkCEckJ6FDqBStTVV0sVzBAFcew44krYR+jfRzeP1IJsyR92bfcHJqjHWj82AHOxt1NZ+L5FO51w12lHsewOjlTjkeM4upLSzctd9dq0RXjjwhQ+AqxEgnwgYz0UYvwgYTwTkfkCSNWpjnY+gohehVrOX9W+m3ys5brW3BZn4i3laL8g0NFT0E2k9QkoWR3rIEh8hbRZH4r4sbRdEGoYu9kpptPjQxb7+ZvPru2/o/HmaKun/Ybr9ZyJfi7OpMj4DFPM1OVtSxm9Q3JupuXTcnvcLUXq3ggVwQV0PkTdslWx5Em8TlrjyiCxHu03B+7d1130CUvtV2Q==";
+    assertEquals(myBlueprint.encode(), ideal_output);
+});
+
+Deno.test("multiples", () => {
+    let myBlueprint: InstanceType<typeof Blueprint> = new Blueprint();
+
+    function create_assembler() {
+        for (let i = 1; i <= 3; i++) {
+            let position = {x: i * 9 , y: 0};
+            let entity = myBlueprint.createEntity("assembling_machine_1", position, Blueprint.UP);
+        let orientations_ideal = ["o", "i", "o", "o", "i", "o", "o", "i", "o", "o", "i", "o"];
+        add_inserters(DEFAULT_ENTITIES.getEntityData().assembling_machine_1, myBlueprint, position, orientations_ideal);
+        }
+    }
+
+    create_assembler();
     let ideal_output = "0eJy9lttugzAMht/F1yCR0JWWy71GVVVArdZSSFAI1SrEuy+BburK1hQNcUkw9hcfftxCLhqsNEkDaQtUKFlDumuhppPMhDsz1wohBTJYQgAyK91TVtdY5oLkKSyz4kwSQwZdACSP+AEp6wKvixKP1JQhCiyMpiKslMA7F7zbB4DSkCEckJ6FDqBStTVV0sVzBAFcew44krYR+jfRzeP1IJsyR92bfcHJqjHWj82AHOxt1NZ+L5FO51w12lHsewOjlTjkeM4upLSzctd9dq0RXjjwhQ+AqxEgnwgYz0UYvwgYTwTkfkCSNWpjnY+gohehVrOX9W+m3ys5brW3BZn4i3laL8g0NFT0E2k9QkoWR3rIEh8hbRZH4r4sbRdEGoYu9kpptPjQxb7+ZvPru2/o/HmaKun/Ybr9ZyJfi7OpMj4DFPM1OVtSxm9Q3JupuXTcnvcLUXq3ggVwQV0PkTdslWx5Em8TlrjyiCxHu03B+7d1130CUvtV2Q==";
     assertEquals(myBlueprint.encode(), ideal_output);
 });
@@ -21,7 +39,7 @@ Deno.test("validate add_inserters middles are inputs", () => {
     let myBlueprint: InstanceType<typeof Blueprint> = new Blueprint();
     myBlueprint.createEntity("assembling_machine_1", { x: 0, y: 0 }, Blueprint.UP);
     let orientations_ideal = ["", "i", "", "", "i", "", "", "i", "", "", "i", ""];
-    add_inserters(DEFAULT_ENTITIES.getEntityData().assembling_machine_1, myBlueprint, orientations_ideal);
+    add_inserters(DEFAULT_ENTITIES.getEntityData().assembling_machine_1, myBlueprint, {x: 0, y: 0}, orientations_ideal);
     // console.log(myBlueprint.encode());
     let ideal_output = "0eJy9lN2KwyAQhd9lrhU2P9tsc7mvUUpJ0qEd0DGoKVtC3n012V1K058tlF6qxzOfHp0eatVha4k9lD1QY9hBuerB0Y4rFef8sUUogTxqEMCVjqPKOdS1It5JXTV7YpQJDAKIt/gFZTKIuxYat9RpiQobb6mRrVF4YpEOawHInjzhhHSrtIDWuCA1HOtFAgHHkQO2ZEOFceXtx/G44U7XaEfZLxy3nQ8+4QZ40oeqfdjPSLt9bTobKdajwFujNjXuqwMZG1XxuLeONcOTE588A8xngOmDgNmzCLN/AmYPAqb3AYkdWh/Mr6R6DjWPNX96rNeZsksvLZ0hvb8Qabqm7F50ixciyYsfcjFjKp7EFObHdlOeNDgBB7RuyucjyYtlWmTLIimSEKGqagy9Cj7/1MPwDT80vzE=";
     assertEquals(myBlueprint.encode(), ideal_output);
@@ -31,7 +49,7 @@ Deno.test("validate add_inserters chest top and bottom only inserting", () => {
     let myBlueprint: InstanceType<typeof Blueprint> = new Blueprint();
     myBlueprint.createEntity("wooden_chest", { x: 0, y: 0 }, Blueprint.UP);
     let orientations_ideal = ["i", "", "i", ""];
-    add_inserters(DEFAULT_ENTITIES.getEntityData().wooden_chest, myBlueprint, orientations_ideal);
+    add_inserters(DEFAULT_ENTITIES.getEntityData().wooden_chest, myBlueprint, {x: 0, y: 0}, orientations_ideal);
     // console.log(myBlueprint.encode());
     let ideal_output = "0eJytk91ugzAMhd/F10EqlImVy73GVFX8WMVScFASuiHEu8+hU9WNbawSN5ESjs/5jOURSt1jZ4k95CNQZdhB/jqCozMXOrz5oUPIgTy2oICLNtzejKmRo6pB52FSQFzjO+TxpFZLW6ypbyPUWHlLVdQZjXcWyXRUgOzJE15RfopU0BknEsMhR8p2CgY5xacmK87zl92n03Divi3RCqC6QXHXBx/pmK96SRulnpHOTWl6G9KPs8Bbo08lNsWFjA2q0OZf7Szwonjmi+KvgOkCMHkQcL8V4X8B9w8CJuuAxA6tF/NfpvodajnWdPOxrjGt/qenjZDkfd6f/G5TFVzQujk4eY7T7JBk+0MWZ7EMRxclyvLBy009TR8nSlRf";
     assertEquals(myBlueprint.encode(), ideal_output);
@@ -41,7 +59,7 @@ Deno.test("validate add_inserters no io", () => {
     let myBlueprint: InstanceType<typeof Blueprint> = new Blueprint();
     myBlueprint.createEntity("assembling_machine_1", { x: 0, y: 0 }, Blueprint.UP);
     // let orientations_ideal = ["", "i", "", "", "i", "", "", "i", "", "", "i", ""];
-    add_inserters(DEFAULT_ENTITIES.getEntityData().assembling_machine_1, myBlueprint);
+    add_inserters(DEFAULT_ENTITIES.getEntityData().assembling_machine_1, {x: 0, y: 0}, myBlueprint);
     // console.log(myBlueprint.encode());
     let ideal_output = "0eJytksFqwzAQRP9lzzLUdsCNj/2NEoJkL/aCtDKSHGqM/72S3JbQ0LSBHrUazbwVs4LSM06OOEC7AnWWPbSvK3gaWOo0C8uE0AIFNCCApUkn6T0apYmHwshuJMaihE0AcY9v0Jab+NXCYE+zKVBjFxx1xWQ1XllU20kAcqBAuCPdixYwWR+lllNeIhCwZA7oycWEfPP04biceTYKXZZ9wvE0h+gTf4B3fUxd43tGGkZlZ5coTlkQnNVnhaO8kHVJlda9t9YNXrHzFd8ADzeA1YOA9X8R1n8ErB8ErH4CjPNckPaqkgIu6HxOrp7LQ3OsmvrYlE0ZU7VUGNsFL1/qbXsHyI70ag==";
     assertEquals(myBlueprint.encode(), ideal_output);
